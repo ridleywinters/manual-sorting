fc8d9c8b3cbb6febca416c9187b01931ebffcea6
Kh4f <khafg050@gmail.com>
2025-02-10 14:32:10 +0300

    feat: add development-only logging function

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 import { Notice, Plugin } from 'obsidian';
 
 
+function debugLog(...args: any[]) {
+	if(process.env.NODE_ENV === "development") {
+		console.log(...args);
+	}
+}
+
 export default class ManualSortingPlugin extends Plugin {
 	async onload() {
 		this.addRibbonIcon('dice', 'Manual Sorting Plugin', () => {
