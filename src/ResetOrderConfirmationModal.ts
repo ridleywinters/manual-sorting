efdd991355adacc0ac3b4b6d16158ad83b33c61e
Kh4f <khafg050@gmail.com>
2025-03-23 15:37:52 +0300

    style(modal): add `manual-sorting-modal` class for more specific `.modal-buttons` styling

--- src/ResetOrderConfirmationModal.ts
+++ src/ResetOrderConfirmationModal.ts
@@ -4,6 +4,7 @@ export class ResetOrderConfirmationModal extends Modal {
     constructor(app: App, onSubmit: () => void) {
         super(app);
         this.setTitle("Manual sorting");
+        this.modalEl.addClass("manual-sorting-modal");
 
         const modalContent = this.contentEl.createEl("div");
         modalContent.createEl("p", { text: "Are you sure you want to reset order to default?" });
